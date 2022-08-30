aaa3f3cf961c8b112d4c5ed29c5b088cf917ef0f
Pramesh <pramesh11172@gmail.com>
2022-08-31 00:18:37 +0930

    changed endpoint for php backend

--- src/Constants/constant.js
+++ src/Constants/constant.js
@@ -1,8 +1,13 @@
 //enpoint python anywhere
-export const baseUrl = "https://tricode.pythonanywhere.com/";
+// export const baseUrl = "https://tricode.pythonanywhere.com/";
 
 //endpoint local computer ip address(python manage.py runserver 0.0.0.0:8000)
 // export const baseUrl = "http://192.168.1.129:8000/";
 
-export const contactListCreate = baseUrl + "contacts/list-create-contacts/";
-export const contactEditDelete = baseUrl + "contacts/edit-delete-contacts/";
+// export const contactListCreate = baseUrl + "contacts/list-create-contacts/";
+// export const contactEditDelete = baseUrl + "contacts/edit-delete-contacts/";
+
+//endpoint php
+export const baseUrl = "http://localhost/bonntech/";
+export const contactListCreate = baseUrl;
+export const contactEditDelete = baseUrl;
